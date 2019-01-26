c7582ce4654bc73c9763d04c5c8cf4d4536b629b
Eddy703 <eddylei070300@gmail.coom>
2019-01-26 21:39:18 +0800

    -Changed order of variable

--- FormSpam.py
+++ FormSpam.py
@@ -1,16 +1,17 @@
 #This py file only works on single text field
-#And this particular file targets a specific form made by so called yw_politics_association on instagram
+#And this particular file targets a specific form made by so called @yw_politics_association on instagram
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+url= 'https://docs.google.com/forms/d/e/1FAIpQLSfPcxRjEbGq5qo5lcHUWZjLcOSeBfScNUH48jMSubgPst-5zQ/viewform'
 #Prompt the user the enter the number of time to spam the form
 num_times=input("Enter the number of time you want to spam: ")
 #Prompt the user to enter the content to spam
 text=input("Enter the text you want to spam: ")
 
-for i in range(1,int(num_times)):
-    driver = webdriver.Chrome()
-    url= 'https://docs.google.com/forms/d/e/1FAIpQLSfPcxRjEbGq5qo5lcHUWZjLcOSeBfScNUH48jMSubgPst-5zQ/viewform'
+for i in range(1,int(num_times)+1):
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe')
     driver.get(url)
     #Goto the Link
     text_field=driver.find_element_by_xpath('//textarea[@class="quantumWizTextinputPapertextareaInput exportTextarea"]')
@@ -20,4 +21,4 @@ for i in range(1,int(num_times)):
     submitbtn.click()
     #close the browser
     driver.close()
-    print(f'Done! Count: {i}.')
+    print(f'Done! {i} '+("times. " if i>1 else "time. "))
