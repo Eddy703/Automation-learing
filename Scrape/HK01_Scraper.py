947a716f17549c955313678a081ac3f1f26b8963
Eddy703 <eddylei070300@gmail.com>
2019-01-31 02:48:42 +0800

    -Can scrape contents now!

---
+++ Scrape/HK01_Scraper.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup as BS
+from os import system
+from time import sleep as Pause
+import requests
+import csv
+import re
+
+str1 = "下載《香港01》App，打開App首頁，撳「＋」選取「01眾樂迷」做你嘅「已選頻道」，每日都睇盡【01眾樂迷】精彩內容啦！即睇 【01眾樂迷專屬頻道】"
+str2 = "下載「香港01」App ，即睇城中熱話："
+print(f'*'*10 + f'HK01 articles scrape(Paragraphs only)' + f'*'*10) #Discrption of the file
+Pause(1)
+system('cls')
+
+r=requests.get("https://www.hk01.com/") #request to get the site/ goto the site
+
+print(f'Connected' if r.status_code == 200 else 'Connection failed') #check if .get succeed or not
+
+data=r.text #get the html file
+soup=BS(data,'lxml') #parse the html
+
+href_lst = set(soup.find_all('a', href=re.compile('/'))) #find everythin that contains hyperlink
+
+urls=[] #initialize an empty list for storing the url for later use (just for this particular file)
+
+uCount=0 #count the relevant links
+with open('./ScrapedContents/url.csv','a+',encoding='utf-8') as f: #create a file to store the contents
+    writer=csv.writer(f) #used to write the row
+
+    inlst=[] #initialize an empty list for writing in csv
+    for item in href_lst: #iterate through the returned list
+
+        if not(re.match('/[A-Za-z]*/[0-9]+/|/[A-Za-z...]|https://', item['href'])): 
+            #condition for filtering out unwanted links (such as /zone/18, /channel/18)
+            url="https://www.hk01.com"+item['href'] #make a complete url instead of just /(*)/(*)/(*)         
+            urls.append(url) 
+            uCount+=1
+            sublst=[item.string, url]
+            inlst.append([item.string, url]) #stores the sublist into a wholelist for actually writing in later
+
+    for subl in inlst:      #writing in !
+        writer.writerow(subl)
+
+f.close()
+
+inlst2=[]
+cCount=0
+with open('./ScrapedContents/content.csv', 'a+', encoding='utf-8') as k:
+    kWriter=csv.writer(k)
+
+    for url in urls: 
+
+        if url== "https://www.hk01.com/": 
+            #another condition for filtering unwanted link, this is for the hyperlink that links back to homepage
+            continue
+
+        c = requests.get(url)
+        content=c.text
+        content_soup=BS(content,'lxml')
+        para_lst=content_soup.find_all('p', class_=re.compile('wa4tvz-0 hmJMOX sc-gqjmRU jTjJUk|u02q31-0 gvqXdj sc-gqjmRU gBjLGB'))
+
+        completeContent=[]
+
+        for para in para_lst:               
+            if (para.string == str2)or (para.string == str1):
+               #condition to remove unwanted strings, these 2 are the promotions of their own company/site
+                continue
+            else:
+                completeContent.append("".join(para.stripped_strings)) 
+                #add seperated paragraphs to a list for joining back to a whole paragraph
+
+        paragraph=''.join(completeContent)
+        print(f'Scraping {url} \n' if c.status_code==200 else f'Error: requests.get failed')
+        inlst2.append([str(url),paragraph])     
+
+    for subls in inlst2:
+        kWriter.writerow(subls)
+        cCount+=1
+
+k.close() #close the file
+
+#Conclusion on running this scraper
+print(f'Done scraping, clearing screen in 6 seconds.')
+Pause(6)
+system('cls')
+print(f'Received {uCount} relevant URLs and scraped {cCount} actual articles.')
